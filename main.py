db1d2085b1ef1dca03254750e747607a22b250e6
ZachJPinet <zachjpinet@gmail.com>
2024-07-19 18:17:25 -0400

    initial setup

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from discord.ext import commands
 
 def run():
     # His settings: DISCORD_API_SECRET = os.getenv("DISCORD_API_TOKEN")
-    print(settings.token)
+    print(settings.TOKEN)
     print("done")
 
 if __name__ == "__main__":
     run()
 else:
     print("uhoh")
-
+token = 'MTI2MzY0ODQwNTY4Nzg5NDA2OQ.Gr05Ew.LodypuRVIKVVEnwrg6kOmIJrDJ00JwkIkqjOnQ'
 print('wtffff')

571562ab07a55ecf61abd079ff6653c93b93919c
ZachJPinet <zachjpinet@gmail.com>
2024-07-19 19:52:25 -0400

    ping and help commands added

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
-# Essential Imports.
+# Essential imports.
 import settings
-
-# Discord imports.
 import discord
 from discord.ext import commands
 
+logger = settings.logging.getLogger("bot")
+
+# This 
 def run():
-    # His settings: DISCORD_API_SECRET = os.getenv("DISCORD_API_TOKEN")
-    print(settings.TOKEN)
-    print("done")
+    intents = discord.Intents.default()
+
+    bot = commands.Bot(command_prefix=">")
+
+    # This gets the bot online when the code is run.
+    @bot.event
+    async def on_ready():
+        logger.info(f"User: {bot.user} (ID: {bot.user.id})")
+
+    # This is the ping command. It returns the bot latency.
+    @bot.command(
+        aliases=['p'],
+        help = "Use the help command for more information.",
+        description = "This returns a message saying 'pong!' and displays the bot latency.",
+        brief = " - returns the bot latency."
+    )
+    async def ping(ctx):
+        await ctx.send("pong!")
+
+    # This is the panini command. It returns panini.
+    @bot.command(
+        enabled=True,
+        hidden=True
+    )
+    async def panini(ctx):
+        await ctx.send("Hell yeah baby it's panini time!")
+
+    bot.run(settings.TOKEN) # , root_logger=True
 
 if __name__ == "__main__":
     run()
-else:
-    print("uhoh")
-token = 'MTI2MzY0ODQwNTY4Nzg5NDA2OQ.Gr05Ew.LodypuRVIKVVEnwrg6kOmIJrDJ00JwkIkqjOnQ'
-print('wtffff')
--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 import os
+import logging
+from logging.config import dictConfig
 from dotenv import load_dotenv
 
 # Loads the dotenv file.
@@ -6,3 +8,47 @@ load_dotenv()
 
 # Gets the bot token stored in the dotenv file.
 TOKEN = os.getenv('token')
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disabled_existing_Loggers": False,
+    "formatters":{
+        "verbose":{
+            "format": "%(levelname)-10s - %(asctime)s - %(module)-15s : %(message)s"
+        },
+        "standard":{
+            "format": "%(levelname)-10s - %(name)-15s : %(message)s"
+        },
+    },
+    "handlers":{
+        "console": {
+            'level': "DEBUG",
+            'class': "logging.StreamHandler",
+            'formatter': "standard"
+        },
+        "console2": {
+            'level': "WARNING",
+            'class': "logging.StreamHandler",
+            'formatter': "standard"
+        },
+        "file": {
+            'level': "INFO",
+            'class': "logging.FileHandler",
+            'filename': "logs/infos.log",
+            'mode': "w",
+            'formatter': "verbose"
+        },
+    },
+    "Loggers":{
+        "bot": {
+            'handlers': ['console'],
+            "level": "INFO",
+            "propagate": False
+        },
+        "discord": {
+            'handlers': ['console2', "file"],
+            "level": "INFO",
+            "propagate": False
+        }
+    }
+}
